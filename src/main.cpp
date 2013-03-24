19a149e9d53f84746ff7deb3e6fa9309f4e956bb
OGURA_Daiki <8hachibee125@gmail.com>
2013-03-24 18:54:22 +0900

    print all cards

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,18 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <boost/foreach.hpp>
+
 
 int main(int argc, char const* argv[])
 {
     // ICardPicture* pict = new Clover();
     // Tramp card = Tramp(1, pict);
-    auto cards = Cardbox::getCards();
-    std::cout << cards[0].toString() << std::endl;
+    std::vector<Tramp> cards = Cardbox::getCards();
+    BOOST_FOREACH(Tramp& card, cards)
+    {
+        std::cout << card.toString() << std::endl;
+    }
     return 0;
 }
 
